cfda3b0b81a5d0e7850900a358569532dfb26c2a
Adelmo Santos <adelmojsantos1985@gmail.com>
2022-10-13 21:11:33 -0300

    Finalizando interface e criando testes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
-import { Envelope, Lock } from 'phosphor-react'
-import { Heading } from './components/Heading'
-import { Text } from './components/Text'
-import { TextInput } from './components/TextInput'
-import { Logo } from './Logo'
+import { SignIn } from './pages/SignIn'
+
 import './styles/global.css'
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
-      <header className='flex flex-col items-center'>
-        <Logo />
-
-        <Heading size='lg' className='mt-4'>Ignite Lab</Heading>
-
-        <Text size='lg' className='text-gray-400 mt-1'>
-          Faça login e comece a usar
-        </Text>
-      </header>
-
-
-      <form className='flex flex-col gap-4 items-stretch w-full max-w-[400px] mt-10'>
-        <label htmlFor="email" className='flex flex-col gap-3'>
-          <Text className='font-semibold'>Endereço de e-mail</Text>
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Envelope />
-            </TextInput.Icon>
-
-            <TextInput.Input id='email' type="email" placeholder='Digite seu e-mail' />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="password" className='flex flex-col gap-3'>
-          <Text className='font-semibold'>Sua senha</Text>
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Lock />
-            </TextInput.Icon>
-
-            <TextInput.Input id='password' type="password" placeholder='*****' />
-          </TextInput.Root>
-        </label>
-      </form>
-    </div>
+    <SignIn />
   )
 }
 
